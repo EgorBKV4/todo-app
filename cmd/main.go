ee18e85b47cfcd238f445d5e01970f3bc4c3f221
egorbukov <bukovegor4444@gmail.com>
2022-04-10 21:59:03 +0300

    lesson 3. Добавил поддержку файлов конфигураций, поднял БД, создал файлы миграции и в них описал структуру БД.

--- cmd/main.go
+++ cmd/main.go
@@ -5,17 +5,27 @@ import (
 	"github.com/EgorBKV4/todo-app/pkg/handler"
 	"github.com/EgorBKV4/todo-app/pkg/repository"
 	"github.com/EgorBKV4/todo-app/pkg/service"
-	"log"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		logrus.Fatalf("error initializing configs: %s", err.Error())
+	}
 
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
-		log.Fatalf("error occured while running http server: %s", err.Error())
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+		logrus.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
