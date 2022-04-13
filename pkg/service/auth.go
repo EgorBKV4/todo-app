5e026565956c8867bc860648d56b1993eab40914
egorbukov <bukovegor4444@gmail.com>
2022-04-13 23:21:49 +0300

    lesson 5. Реализовал регистрацию пользователей.

---
+++ pkg/service/auth.go
@@ -0,0 +1,32 @@
+package service
+
+import (
+	"crypto/sha1"
+	"fmt"
+	"github.com/EgorBKV4/todo-app"
+	"github.com/EgorBKV4/todo-app/pkg/repository"
+)
+
+const (
+	salt = "hgfjhg7gujhll"
+)
+
+type AuthService struct {
+	repo repository.Authorization
+}
+
+func NewAuthService(repo repository.Authorization) *AuthService {
+	return &AuthService{repo: repo}
+}
+
+func (s *AuthService) CreateUser(user todo.User) (int, error) {
+	user.Password = generatePasswordHash(user.Password)
+	return s.repo.CreateUser(user)
+}
+
+func generatePasswordHash(password string) string {
+	hash := sha1.New()
+	hash.Write([]byte(password))
+
+	return fmt.Sprintf("%x", hash.Sum([]byte(salt)))
+}
