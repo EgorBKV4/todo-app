2c221d630cbd3daf45a659991c103cb1a3bc3993
egorbukov <bukovegor4444@gmail.com>
2022-04-14 12:27:51 +0300

    lesson 6. Добавил аутентификацию пользователя.

--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -5,11 +5,20 @@ import (
 	"fmt"
 	"github.com/EgorBKV4/todo-app"
 	"github.com/EgorBKV4/todo-app/pkg/repository"
+	"github.com/dgrijalva/jwt-go"
+	"time"
 )
 
 const (
-	salt = "hgfjhg7gujhll"
+	salt       = "hgfjhg7gujhll"
+	signingKey = "qrkjdfsdsdfsd"
+	tokenTTL   = 12 * time.Hour
 )
+
+type tokenClaims struct {
+	jwt.StandardClaims
+	UserID int `json:"user_id"`
+}
 
 type AuthService struct {
 	repo repository.Authorization
@@ -22,6 +31,23 @@ func NewAuthService(repo repository.Authorization) *AuthService {
 func (s *AuthService) CreateUser(user todo.User) (int, error) {
 	user.Password = generatePasswordHash(user.Password)
 	return s.repo.CreateUser(user)
+}
+
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	user, err := s.repo.GetUser(username, generatePasswordHash(password))
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(tokenTTL).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+		user.ID,
+	})
+
+	return token.SignedString([]byte(signingKey))
 }
 
 func generatePasswordHash(password string) string {
